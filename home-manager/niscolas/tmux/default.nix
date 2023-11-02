ea2fac3ab973b1286c609223f218463e54378769
niscolas <niscolas@proton.me>
2023-11-02 17:15:57 -0300

    feat(tmux): updates config

--- home-manager/niscolas/tmux/default.nix
+++ home-manager/niscolas/tmux/default.nix
@@ -3,8 +3,16 @@
 {
     programs.tmux = {
         enable = true;
+
+        baseIndex = 1;
+        escapeTime = 0;
+        keyMode = "vi";
+        shortcut = "Space";
+
         plugins = [
             pkgs.tmuxPlugins.sensible
+            pkgs.tmuxPlugins.vim-tmux-navigator
+            pkgs.tmuxPlugins.gruvbox
         ];
     };
 }
