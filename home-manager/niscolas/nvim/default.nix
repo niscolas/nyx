42f44d77f3bc2995876aac3f5518435782295356
niscolas <niscolas@proton.me>
2023-11-24 13:38:30 -0300

    feat(nvim): c++ setup

--- home-manager/niscolas/nvim/default.nix
+++ home-manager/niscolas/nvim/default.nix
@@ -2,6 +2,7 @@
 
 {
   home.packages = with pkgs; [
+      clang-tools
       csharp-ls
       dotnet-sdk_8
       fd
@@ -10,11 +11,10 @@
       lua-language-server
       neovim
       nil
+      nodePackages.bash-language-server
       ripgrep
       stylua
       vscode-langservers-extracted
-      libclang
-      nodePackages.bash-language-server
       yaml-language-server
   ];
 
