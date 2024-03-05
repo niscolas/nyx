11b86baa3401e7c88ae5a149e1ecc6fbdbbb7dd8
niscolas <niscolas@proton.me>
2024-03-05 00:53:09 -0300

    579

--- packages/default.nix
+++ packages/default.nix
@@ -15,4 +15,21 @@
       ${git}/bin/git commit -am "$gen"
       popd
     '';
+
+  hm-switch = with pkgs;
+    pkgs.writeScriptBin "hm-switch" ''
+      set -e
+      pushd ~/bonfire/nyx/
+      ${git}/bin/git diff -U0 *.nix
+      echo "Home Manager switching..."
+      home-manager switch --show-trace --flake . &>hm-switch.log || (
+       cat hm-switch.log | ${ripgrep}/bin/rg --color error && false)
+
+      set +e
+      gen=$(home-manager generations | head -n 1 | awk '{print $5}')
+      set -e
+
+      ${git}/bin/git commit -am "$gen"
+      popd
+    '';
 }
