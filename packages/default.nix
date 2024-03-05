b5e575d96f8bfa1e218525afb578d79fdf413349
niscolas <niscolas@proton.me>
2024-03-05 00:46:17 -0300

    922 current  2024-03-05 00:46:11  23.11.20240110.3dc440f  6.1.70-xanmod1                          eco steam_off turbo

--- packages/default.nix
+++ packages/default.nix
@@ -1,11 +1,18 @@
 {pkgs, ...}: {
   rebuild = with pkgs;
     pkgs.writeScriptBin "rebuild" ''
+      set -e
+      pushd ~/bonfire/nyx/
       ${git}/bin/git diff -U0 *.nix
       echo "NixOS Rebuilding..."
       sudo nixos-rebuild switch --flake . &>nixos-switch.log || (
-       cat nixos-switch.log | grep --color error && false)
-      gen=$(nixos-rebuild list-generations | grep current)
+       cat nixos-switch.log | ${ripgrep}/bin/rg --color error && false)
+
+      set +e
+      gen=$(nixos-rebuild list-generations | ${ripgrep}/bin/rg current)
+      set -e
+
       ${git}/bin/git commit -am "$gen"
+      popd
     '';
 }
