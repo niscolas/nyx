eb569e7c0ce00dd16259601d7719f5db0aa28cc5
niscolas <niscolas@proton.me>
2024-03-07 12:34:33 -0300

    Home Manager Switch: 584

--- homes/x86_64-linux/izalith/niscolas/ssh/default.nix
+++ homes/x86_64-linux/izalith/niscolas/ssh/default.nix
@@ -4,6 +4,31 @@
   pkgs,
   ...
 }: {
-  programs.ssh.enable = true;
-  home.file.".ssh/config".source = ./config;
+  programs.ssh = {
+    enable = true;
+    # addKeysToAgent = "yes";
+
+    matchBlocks = {
+      nokron = {
+        host = "nokron";
+        hostname = "139.144.170.177";
+        user = "niscolas";
+      };
+
+      liurnia = {
+        host = "nokron";
+        hostname = "192.168.100.12";
+        user = "niscolas";
+      };
+
+      github = {
+        host = "github.com";
+        hostname = "github.com";
+        user = "git";
+        identityFile = "~/.ssh/niscolas_github";
+      };
+    };
+  };
+
+  # home.file.".ssh/config".source = ./config;
 }
