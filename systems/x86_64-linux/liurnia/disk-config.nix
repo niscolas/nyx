fe41d24dcd6e9147fe7cc2756bc7860b4a3cd88a
niscolas <niscolas@proton.me>
2024-03-12 02:31:05 -0300

    feat: adds initial Liurnia config

---
+++ systems/x86_64-linux/liurnia/disk-config.nix
@@ -0,0 +1,134 @@
+{...}: let
+  systemPoolName = "zsystem";
+  storagePool = {
+    name = "zstorage";
+    mountpoint = "/${storagePool.name}";
+    data = {
+      name = "data";
+      mountpoint = "${storagePool.mountpoint}/${storagePool.data.name}";
+    };
+    backup = {
+      name = "backup";
+      mountpoint = "${storagePool.mountpoint}/${storagePool.backup.name}";
+    };
+    temp = {
+      name = "temp";
+      mountpoint = "${storagePool.mountpoint}/${storagePool.temp.name}";
+    };
+  };
+in {
+  disko.devices = {
+    disk = {
+      ssd = {
+        type = "disk";
+        device = "/dev/nvme0n1";
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              size = "64M";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+              };
+            };
+            zfs = {
+              size = "100%";
+              content = {
+                type = "zfs";
+                pool = "${systemPoolName}";
+              };
+            };
+          };
+        };
+      };
+      hdd1 = {
+        type = "disk";
+        device = "/dev/sda";
+        content = {
+          type = "gpt";
+          partitions = {
+            zfs = {
+              size = "100%";
+              content = {
+                type = "zfs";
+                pool = "${storagePool.name}";
+              };
+            };
+          };
+        };
+      };
+      hdd2 = {
+        type = "disk";
+        device = "/dev/sdb";
+        content = {
+          type = "gpt";
+          partitions = {
+            zfs = {
+              size = "100%";
+              content = {
+                type = "zfs";
+                pool = "${storagePool.name}";
+              };
+            };
+          };
+        };
+      };
+    };
+    zpool = {
+      ${systemPoolName} = {
+        type = "zpool";
+        rootFsOptions = {
+          "com.sun:auto-snapshot" = "false";
+          compression = "zstd";
+          encryption = "aes-256-gcm";
+        };
+        mountpoint = "/";
+        postCreateHook = "zfs snapshot ${systemPoolName}@blank";
+
+        datasets = {
+          system_fs = {
+            type = "zfs_fs";
+            mountpoint = "/system_fs";
+            options = {
+              "com.sun:auto-snapshot" = "true";
+            };
+          };
+        };
+      };
+
+      ${storagePool.name} = {
+        type = "zpool";
+        mode = "mirror";
+        rootFsOptions = {
+          "com.sun:auto-snapshot" = "false";
+          compression = "zstd";
+          encryption = "aes-256-gcm";
+        };
+        mountpoint = "/${storagePool.name}";
+        postCreateHook = "zfs snapshot ${storagePool.name}@blank";
+
+        datasets = {
+          "${storagePool.data.name}" = {
+            type = "zfs_fs";
+            mountpoint = "${storagePool.data.mountpoint}";
+            options."com.sun:auto-snapshot" = "true";
+          };
+
+          "${storagePool.backup.name}" = {
+            type = "zfs_fs";
+            mountpoint = "${storagePool.backup.mountpoint}";
+            options."com.sun:auto-snapshot" = "true";
+          };
+
+          "${storagePool.temp.name}" = {
+            type = "zfs_fs";
+            mountpoint = "${storagePool.temp.mountpoint}";
+          };
+        };
+      };
+    };
+  };
+}
