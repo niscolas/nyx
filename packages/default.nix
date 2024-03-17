357dc8fe132960a6e67fdaeae28b5c9a9b4ad40e
niscolas <niscolas@proton.me>
2024-03-17 13:04:52 -0300

    feat(nix): updates packages scripts

--- packages/default.nix
+++ packages/default.nix
@@ -1,49 +1,58 @@
-{pkgs, ...}: {
-  rebuild = with pkgs;
-    pkgs.writeScriptBin "rebuild" ''
+{pkgs, ...}: let
+  configDir = "/home/niscolas/bonfire/nyx";
+in {
+  rebuild = pkgs.writeScriptBin "rebuild" ''
+    sudo nixos-rebuild switch --show-trace --flake ${configDir}
+  '';
+
+  rebuild-git = with pkgs;
+    pkgs.writeScriptBin "rebuild-git" ''
       set -e
-      pushd ~/bonfire/nyx/
       ${git}/bin/git diff -U0 *.nix
       echo "NixOS Rebuilding..."
-      sudo nixos-rebuild switch --flake . &>nixos-switch.log || (
+      sudo nixos-rebuild switch --flake ${configDir} &>nixos-switch.log || (
        cat nixos-switch.log | grep --color error && false)
 
       set +e
       gen=$(nixos-rebuild list-generations | grep current | awk '{print $1}')
       set -e
 
-      ${git}/bin/git add systems/x86_64-linux/izalith
-      ${git}/bin/git add homes/x86_64-linux/izalith
-      ${git}/bin/git add modules packages flake.nix flake.lock
+      ${git}/bin/git add ${configDir}/systems/x86_64-linux/izalith
+      ${git}/bin/git add ${configDir}/homes/x86_64-linux/izalith
+      ${git}/bin/git add ${configDir}/modules
+      ${git}/bin/git add ${configDir}/packages
+      ${git}/bin/git add ${configDir}/flake.nix
+      ${git}/bin/git add ${configDir}/flake.lock
+
       ${git}/bin/git commit -m "NixOS Switch: $gen"
-      popd
     '';
 
-  hm-switch-no-git = pkgs.writeScriptBin "hm-switch-no-git" ''
-    set -e
-    echo "Home Manager switching..."
-    home-manager switch --show-trace --flake . &>hm-switch.log || (
-     cat hm-switch.log | grep --color error && false)
+  hm-switch = pkgs.writeScriptBin "hm-switch" ''
+    home-manager switch --show-trace --flake ${configDir}
   '';
 
-  hm-switch = with pkgs;
+  hm-switch-git = with pkgs;
     pkgs.writeScriptBin "hm-switch" ''
       set -e
-      pushd ~/bonfire/nyx/
-      ${git}/bin/git diff -U0 *.nix
+      ${git}/bin/git diff -U0 ${configDir}/*.nix
       echo "Home Manager switching..."
-      home-manager switch --show-trace --flake . &>hm-switch.log || (
+      home-manager switch --show-trace --flake ${configDir} &>hm-switch.log || (
        cat hm-switch.log | grep --color error && false)
       gen=$(home-manager generations | head -n 1 | awk '{print $5}')
       ${git}/bin/git commit -am "Home Manager Switch: $gen"
-      popd
     '';
 
   deploy-liurnia = pkgs.writeScriptBin "deploy-liurnia" ''
-    nix run github:nix-community/nixos-anywhere -- --flake .#liurnia root@liurnia
+    nix run github:nix-community/nixos-anywhere -- --flake ${configDir}#liurnia root@liurnia
   '';
 
   rebuild-liurnia-remote = pkgs.writeScriptBin "rebuild-liurnia-remote" ''
-    nixos-rebuild switch --flake .#liurnia --target-host "root@liurnia"
+    nixos-rebuild switch --flake ${configDir}#liurnia --target-host "root@liurnia"
   '';
+
+  # edit-liurnia-secrets = pkgs.writeScriptBin "edit-liurnia-secrets" ''
+  #   SOPS_AGE_KEY_FILE=~/.config/sops/age/liurnia_keys.txt \
+  #       ${pkgs.sops}/bin/sops \
+  #       ${../systems/x86_64-linux/liurnia/secrets/secrets.yaml}
+  # '';
 }
