f1268f572ae4b55b9769bdcddd1eb92e393f9511
niscolas <niscolas@proton.me>
2024-03-25 22:32:02 -0300

    feat(tmux, nvim, alacritty): proper colors and undercurl!

--- modules/home-manager/tmux/default.nix
+++ modules/home-manager/tmux/default.nix
@@ -18,12 +18,26 @@ in {
       escapeTime = 0;
       keyMode = "vi";
       shortcut = "Space";
+      terminal = "tmux-256color";
 
       plugins = [
         pkgs.tmuxPlugins.sensible
         pkgs.tmuxPlugins.vim-tmux-navigator
         pkgs.tmuxPlugins.gruvbox
       ];
+
+      extraConfig =
+        /*
+        bash
+        */
+        ''
+          # https://github.com/neovim/neovim/issues/21182#issuecomment-1885036257
+          # Proper colors
+          set-option -sa terminal-features ',alacritty:RGB' # Makes sure that colors in tmux are the same as without tmux
+
+          # Undercurl
+          set-option -ga terminal-features ",alacritty:usstyle"
+        '';
     };
   };
 }
